cf37a13e43392fcfdd7b3f0754209c3886fbdca1
NilRom <79469939+NilRom@users.noreply.github.com>
2022-06-05 13:21:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit as st
 st.write("Hello World")
+box_1 = st.selectbox('What do you want to do' , ['NN Playground','Simon Style GAN'])
+if box_1 == 'Simon Style GAN':
+  st.write('https://openai.com/dall-e-2/')
